b2a2081f6e7c764a9fdb7faa32fd44ff866aaafc
Bytewright <bytewright@t-online.de>
2016-10-12 19:06:14 +0200

    new jobs for training

--- auto_trainer/my_tools/conv_filter_chain_calc.py
+++ auto_trainer/my_tools/conv_filter_chain_calc.py
@@ -1,23 +1,13 @@
-conv_filter = [5, 4]
-conv_stride = [2, 2]
-conv_filter = [11]
-conv_stride = [4]
+conv_filter = [7, 3, 3]
+conv_stride = [3, 1, 2]
+padding =     [3, 0, 0]
+#conv_filter = [11]
+#conv_stride = [4]
 size = 227
 output_vars = 0
-for i in range(len(conv_filter)):
-    start = 0
-    end = start + conv_filter[i]
-    output_vars = 1
-    while end < size:
-        print('{}\t({}, {})'.format(output_vars, start, end))
-        if end + conv_stride[i] > size:
-            break
-        start += conv_stride[i]
-        end += conv_stride[i]
-        output_vars+=1
-    if i < len(conv_filter)-1:
-        size = output_vars
-        output_vars = 1
-        start = 0
+for F,P,S in zip(conv_filter,padding,conv_stride):
+    out_dim = (float(size)-float(F)+2.0*float(P))/float(S)+1.0
+    print 'F {},P {},S {}=\t{}x{}'.format(F, P, S, int(out_dim),int(out_dim))
+    size = out_dim
+print 'outsize: {}x{}'.format(int(out_dim),int(out_dim))
 
-print('endsize:\t{}x{}'.format(output_vars, output_vars))
