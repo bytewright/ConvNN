acfed4c87808529524a4c4f6c940952b1a833820
Bytewright <bytewright@t-online.de>
2016-10-12 19:21:19 +0200

    new job file

--- auto_trainer/my_tools/testing.py
+++ auto_trainer/my_tools/testing.py
@@ -1,10 +1,13 @@
 import os
 import json
 
-output_dir = '/home/ellerch/caffeProject/auto_trainer_output'
+#output_dir = '/home/ellerch/caffeProject/auto_trainer_output'
+output_dir = 'H:\\Entwicklung\\ConvNN\\auto_trainer\\my_tools'
 stats_dict = {'0':'1', 'abc':['a','b','c'], '123':{'x':'y','g':'h'}}
-log_path = os.path.join(output_dir, "stats.json")
-json.dump(stats_dict, open(log_path, 'w'), sort_keys=True, indent=4, separators=(',', ': '))
-
+stats_dict = {'0':{'name':'bla', 'solver_path':'bla', 'model_path':'bla'}, '1':{'name':'bla2', 'solver_path':'bla', 'model_path':'bla'}}
+log_path = os.path.join(output_dir, "jobs.json")
+#json.dump(stats_dict, open(log_path, 'w'), sort_keys=True, indent=4, separators=(',', ': '))
+log_path = 'H:\\Entwicklung\\ConvNN\\jobs\\jobs.json'
 a=json.load(open(log_path, 'r'))
-print a.keys()
+for job in a:
+    print a[job]
