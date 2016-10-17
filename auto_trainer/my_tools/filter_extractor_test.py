b44e509d8a837659e1b2f0dd7b837f34a87efea2
Bytewright <bytewright@t-online.de>
2016-10-17 19:33:14 +0200

    filter extractor updated

--- auto_trainer/my_tools/filter_extractor_test.py
+++ auto_trainer/my_tools/filter_extractor_test.py
@@ -7,33 +7,34 @@ from scipy.misc import toimage
 # 1. load network
 network_path = '/home/ellerch/caffeProject/auto_trainer_output/_gute_runs/alexnet_80k/job0/deploy_alexnet_places365.prototxt'
 weight_path = '/home/ellerch/caffeProject/auto_trainer_output/_gute_runs/alexnet_80k/job0/_iter_75000.caffemodel'
+output_path = '/home/ellerch/caffeProject/auto_trainer_output/_gute_runs/alexnet_80k/'
+
 net = caffe.Net(network_path, weight_path, caffe.TEST)
 
 # 2. get filters
 for name, dim in [(k, v[0].data.shape) for k, v in net.params.items()]:
     print '{}, Dims:{}'.format(name, dim)
-# 96 x 11x11 x3
-#nice_edge_detectors = net.blobs['conv1']
-#higher_level_filter = net.blobs['fc7']
+    # 96 x 11x11 x3
+    if 'conv' not in name:
+        continue
+    filters = net.params[name][0].data
+    print np.shape(filters)
+    filter_size = filters[0][0][0]
+    # 3. compose new image
+    comp_im = Image.new("RGB", ((filter_size + 1) * 10, (filter_size + 1) * 10), "white")
+    offset_x = 0
+    offset_y = 0
+    for i in range(np.shape(filters)[0]):
+        comp_im.paste(toimage(filters[i]),
+                      (offset_x, offset_y))
+        offset_x += filter_size + 1
+        if offset_x > (filter_size + 1) * 10:
+            offset_x = 0
+            offset_y += filter_size + 1
 
-filters = net.params['conv1'][0].data
-print np.shape(filters)
-
-# 3. compose new image
-path = '/home/ellerch/caffeProject/auto_trainer_output/_gute_runs/alexnet_80k/con1_filters/'
-comp_im = Image.new("RGB", (200, 200), "white")
-offset_x = 0
-offset_y = 0
-for i in range(np.shape(filters)[0]):
-    comp_im.paste(toimage(filters[i]),
-                  (offset_x, offset_y))
-    offset_x += 12
-    if offset_x > 120:
-        offset_x = 0
-        offset_y += 12
-
-# 4. save to path
-comp_im.save(path + 'conv1_filters.png')
+    # 4. save to path
+    comp_im = comp_im.resize((400, 400), Image.ANTIALIAS)
+    comp_im.save(output_path + name + '_filters.png')
 #w, h = 512, 512
 #data = np.zeros((h, w, 3), dtype=np.uint8)
 #data[256, 256] = [255, 0, 0]
