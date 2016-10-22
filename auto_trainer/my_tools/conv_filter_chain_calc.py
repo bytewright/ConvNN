b344d3f708be4b2e75c4d42586dbd6a6242de6a2
Bytewright <bytewright@t-online.de>
2016-10-22 11:42:41 +0200

    added alex inception job

--- auto_trainer/my_tools/conv_filter_chain_calc.py
+++ auto_trainer/my_tools/conv_filter_chain_calc.py
@@ -1,9 +1,10 @@
-conv_filter = [7, 3, 3]
-conv_stride = [3, 1, 2]
-padding =     [3, 0, 0]
+conv_filter = [1, 3]
+conv_stride = [1, 2]
+padding =     [0, 1]
 #conv_filter = [11]
 #conv_stride = [4]
-size = 227
+size = 13
+# 256, 13, 13]
 output_vars = 0
 for F,P,S in zip(conv_filter,padding,conv_stride):
     out_dim = (float(size)-float(F)+2.0*float(P))/float(S)+1.0
