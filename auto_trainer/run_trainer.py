9aa640c7c0bcf170a3c0f68569cb40c04e62f23b
Bytewright <bytewright@t-online.de>
2016-10-25 14:22:47 +0200

    better path handling for all files

--- auto_trainer/run_trainer.py
+++ auto_trainer/run_trainer.py
@@ -88,8 +88,6 @@ def get_next_job(jobs_file):
             log.info('ignoring job: ' + jobs_dict[tmp_job]['name'])
             continue
         if jobs_dict[tmp_job]['name'] not in finished_job_names:
-            #log.debug(jobs_dict[tmp_job]['name'] + ' not in:')
-            #log.debug(finished_job_names)
             checked_job = check_job(jobs_dict[tmp_job], log)
             if checked_job is not None:
                 log.info('jobs completed: {}, new job: {}'.format(finished_job_names.__len__(),
@@ -122,19 +120,17 @@ def train_network(job):
     try:
         # generate image of NN
         draw_job_net(job['solver_path'],
-                     os.path.join(job['snapshot_path'], job['name'] + '_net.png'), log)
+                     os.path.join(job['snapshot_path'], job['name'] + '_net.png'), '/home/ellerch/bin/caffe/python/')
 
         # training is done, write log and other output
         generate_parsed_splitted_logs(job['caffe_log_path'],
-                                      job['snapshot_path'], log)
+                                      job['snapshot_path'])
 
         draw_job_plot(job['caffe_log_path'],
-                      os.path.join(job['snapshot_path'], job['name'] + '_training_plot.png'),
-                      log)
+                      os.path.join(job['snapshot_path'], job['name'] + '_training_plot.png'))
 
         draw_job_plot2(os.path.join(job['snapshot_path'], "parsed_caffe_log.test"),
-                       os.path.join(job['snapshot_path'], job['name'] + '_better_training_plot.png'),
-                       log)
+                       os.path.join(job['snapshot_path'], job['name'] + '_better_training_plot.png'))
         acc, training_loss = get_avg_acc_and_loss(os.path.join(job['snapshot_path'], "parsed_caffe_log.test"))
         thread_stats = train_thread.get_stats()
         job['accuracy'] = acc
@@ -144,7 +140,7 @@ def train_network(job):
         # get best caffemodel
         weights_path = get_best_caffemodel(job['snapshot_path'])
         if weights_path is not None:
-            extract_filters(job['model_path'], weights_path, job['snapshot_path'], log)
+            extract_filters(job['model_path'], weights_path, job['snapshot_path'])
         else:
             log.error('Could not find a caffemodel for solver in {}, '
                       'no filters extracted.'.format(job['snapshot_path']))
@@ -203,56 +199,43 @@ if __name__ == '__main__':
     dir_name = datetime.datetime.now().strftime('%Y-%m-%d_%Hh-%Mm-%Ss') + '_experiment'
     output_path = os.path.join(output_dir, dir_name)
     os.makedirs(output_path)
+    fileHandler = logging.FileHandler(os.path.join(output_path, 'auto_trainer.log'))
+    fileHandler.setFormatter(logFormatter)
+    log.addHandler(fileHandler)
     log.info('output dir for this run:\n{}'.format(output_path))
+
     job_file_path = os.path.join(os.path.dirname(__file__), '..', args.jobs_file)
     if not os.path.isfile(job_file_path):
         log.error('jobfile not found at:\n{}'.format(job_file_path))
-    else:
-        log.debug('jobfile found at:{}'.format(job_file_path))
-    test_log('eureka!')
-    if False:
-        fileHandler = logging.FileHandler(os.path.join(output_path, 'auto_trainer.log'))
-        fileHandler.setFormatter(logFormatter)
-        log.addHandler(fileHandler)
+        sys.exit()
 
-        finished_job_names = []
-        while True:
-            # load new job from jobs.json
-            job, do_work = get_next_job(args.jobs_file)
-            if not do_work:
-                break
-            job['output_dir'] = generate_output_directory(job['solver_path'],
-                                                          job['model_path'],
-                                                          job['snapshot_path'],
-                                                          log)
+    finished_job_names = []
+    while True:
+        # load new job from jobs.json
+        job, do_work = get_next_job(job_file_path)
+        if not do_work:
+            log.info('Couldn\'t find new job, ending script')
+            break
+        job['output_dir'] = generate_output_directory(job['solver_path'],
+                                                      job['model_path'],
+                                                      job['snapshot_path'])
 
-            # run training for job
-            duration, completed = train_network(job)
-            job['job_duration'] = duration
-            job['completed'] = completed
-            finished_job_names.append(job['name'])
+        # run training for job
+        duration, completed = train_network(job)
+        job['job_duration'] = duration
+        job['completed'] = completed
+        finished_job_names.append(job['name'])
 
-            # cleanup
-            # after training, post stats
-            if job['completed']:
-                move_all_files_from_to(job['snapshot_path'], os.path.join(output_path, job['name']))
-                #if os.path.exists(os.path.join(output_path, os.path.basename(os.path.dirname(job['snapshot_path'])))):
-                #    log.error('there is already a completed job with name {} in output dir, '
-                #              'please move manually from tmp dir.'.format(os.path.basename(job['snapshot_path'])))
-                #else:
-                #    shutil.move(job['snapshot_path'], output_path)
-                log.info('Job {}: completed in {}'.format(job['name'], job['job_duration']))
-            else:
-                move_all_files_from_to(job['snapshot_path'], os.path.join(output_path, job['name']+'_failed'))
-                #path = job['snapshot_path']
-                #if job['snapshot_path'].endswith('/'):
-                #    path = job['snapshot_path'][:-1]
-                #log.debug('trying to rename \n{}\nto:\n{}\n'.format(path, path + '_failed'))
-                #os.rename(path, path + '_failed')
-                #shutil.move(path + '_failed', output_path)
-                log.info('Job {}: failed in {}'.format(job['name'], job['job_duration']))
+        # cleanup
+        # after training, post stats
+        if job['completed']:
+            move_all_files_from_to(job['snapshot_path'], os.path.join(output_path, job['name']))
+            log.info('Job {}: completed in {}'.format(job['name'], job['job_duration']))
+        else:
+            move_all_files_from_to(job['snapshot_path'], os.path.join(output_path, job['name']+'_failed'))
+            log.info('Job {}: failed in {}'.format(job['name'], job['job_duration']))
 
-        log.info('all jobs completed')
-        log.info('cleaning up tmp dir')
-        if os.path.exists(os.path.join(args.output_path, 'tmp')):
-            os.rmdir(os.path.join(args.output_path, 'tmp'))
+    log.info('all jobs completed')
+    log.info('cleaning up tmp dir')
+    if os.path.exists(os.path.join(output_dir, 'tmp')):
+        os.rmdir(os.path.join(output_dir, 'tmp'))
--- auto_trainer/trainer_utils.py
+++ auto_trainer/trainer_utils.py
@@ -10,57 +10,8 @@ from PIL import Image
 import numpy as np
 from scipy.misc import toimage
 
-CAFFE_TOOL_PATH = '/home/ellerch/bin/caffe/python/'
-MY_TOOLS_PATH = '/home/ellerch/caffeProject/auto_trainer/caffe_tools/'
-
-
-def test_log(bla):
-    logging.debug('blabla:'+bla)
-
-def get_networks_from_file(jobs_file_path, log):
-    job_list = []
-    # open txt with network names/paths
-    with open(jobs_file_path, "r") as jobsfile:
-        for job_desc in jobsfile:
-            job_desc = job_desc.rstrip()  # remove '\n' at end of line
-            if job_desc.startswith('#'):
-                continue
-            if not os.path.isfile(os.path.join(job_desc, 'solver.prototxt')):
-                log.error('no solver.prototxt in \n"{}",\nskipping job'.format(job_desc))
-                continue
-            job_list.append(job_desc)
-    return job_list
-
-
-def check_job(job, log):
-    if job['ignore']:
-        log.info('ignoring job: ' + job['name'])
-        return None
-    if 'solver_path' not in job:
-        log.error('no valid solver_path key in job "{}",\nskipping job'.format(job))
-        return None
-    if not os.path.isfile(job['solver_path']):
-        log.error('no solver at \n"{}",\nskipping job'.format(job['solver_path']))
-        return None
-    net_path = ''
-    snapshot_path = ''
-    with open(job['solver_path'], 'r') as search:
-        for line in search:
-            line = line.rstrip()  # remove '\n' at end of line
-            if line.startswith('snapshot_prefix: '):
-                snapshot_path = line.replace('snapshot_prefix: ', '').replace('"', '')
-            if line.startswith('net: '):
-                net_path = line.replace('net: ', '').replace('"', '')
-    if net_path is not '' and snapshot_path is not '':
-        job['model_path'] = net_path
-        job['snapshot_path'] = snapshot_path
-    else:
-        log.error('no valid model_path or snapshot_path in {}-solver, skipping job'.format(job['name']))
-        return None
-    if not os.path.isfile(job['model_path']):
-        log.error('model_path in {}-solver is no file, skipping job'.format(job['name']))
-        return None
-    return job
+#CAFFE_TOOL_PATH = '/home/ellerch/bin/caffe/python/'
+#MY_TOOLS_PATH = '/home/ellerch/caffeProject/auto_trainer/caffe_tools/'
 
 
 def get_args():
@@ -78,11 +29,57 @@ def get_args():
     return args
 
 
-def extract_filters(network_path, weight_path, output_path, log):
+def get_networks_from_file(jobs_file_path):
+    job_list = []
+    # open txt with network names/paths
+    with open(jobs_file_path, "r") as jobsfile:
+        for job_desc in jobsfile:
+            job_desc = job_desc.rstrip()  # remove '\n' at end of line
+            if job_desc.startswith('#'):
+                continue
+            if not os.path.isfile(os.path.join(job_desc, 'solver.prototxt')):
+                logging.error('no solver.prototxt in \n"{}",\nskipping job'.format(job_desc))
+                continue
+            job_list.append(job_desc)
+    return job_list
+
+
+def check_job(job):
+    if job['ignore']:
+        logging.info('ignoring job: ' + job['name'])
+        return None
+    if 'solver_path' not in job:
+        logging.error('no valid solver_path key in job "{}",\nskipping job'.format(job))
+        return None
+    if not os.path.isfile(job['solver_path']):
+        logging.error('no solver at \n"{}",\nskipping job'.format(job['solver_path']))
+        return None
+    net_path = ''
+    snapshot_path = ''
+    with open(job['solver_path'], 'r') as search:
+        for line in search:
+            line = line.rstrip()  # remove '\n' at end of line
+            if line.startswith('snapshot_prefix: '):
+                snapshot_path = line.replace('snapshot_prefix: ', '').replace('"', '')
+            if line.startswith('net: '):
+                net_path = line.replace('net: ', '').replace('"', '')
+    if net_path is not '' and snapshot_path is not '':
+        job['model_path'] = net_path
+        job['snapshot_path'] = snapshot_path
+    else:
+        logging.error('no valid model_path or snapshot_path in {}-solver, skipping job'.format(job['name']))
+        return None
+    if not os.path.isfile(job['model_path']):
+        logging.error('model_path in {}-solver is no file, skipping job'.format(job['name']))
+        return None
+    return job
+
+
+def extract_filters(network_path, weight_path, output_path):
     if not os.path.exists(weight_path):
-        log.error('weights not found!: ' + weight_path)
+        logging.error('weights not found!: ' + weight_path)
         return
-    log.info('extracting filters')
+    logging.info('extracting filters')
     filter_extract_script = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                          'my_tools',
                                          'filter_extractor.py')
@@ -96,17 +93,17 @@ def extract_filters(network_path, weight_path, output_path, log):
         time.sleep(2)
         returncode = process.returncode
     if returncode is not 0:
-        log.error('extractor return code: '+str(process.returncode))
-        log.error(output)
+        logging.error('extractor return code: '+str(process.returncode))
+        logging.error(output)
     else:
-        log.info('extractor exited successfully (code {})'.format(returncode))
+        logging.info('extractor exited successfully (code {})'.format(returncode))
 
 
-def draw_job_plot(caffe_log_path, output_file, log):
+def draw_job_plot(caffe_log_path, output_file):
     if not os.path.exists(caffe_log_path):
-        log.error('caffe logfile not found!')
+        logging.error('caffe logfile not found!')
         return
-    log.info('plotting learning curve as png')
+    logging.info('plotting learning curve as png')
     plot_script = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'progress_plot.py')
     process = subprocess.Popen(['python', plot_script, caffe_log_path, output_file],
                                stdout=subprocess.PIPE,
@@ -118,17 +115,17 @@ def draw_job_plot(caffe_log_path, output_file, log):
         time.sleep(2)
         returncode = process.returncode
     if returncode is not 0:
-        log.error('plotter return code: '+str(process.returncode))
-        log.error(output)
+        logging.error('plotter return code: '+str(process.returncode))
+        logging.error(output)
     else:
-        log.info('plotter exited successfully (code {})'.format(returncode))
+        logging.info('plotter exited successfully (code {})'.format(returncode))
 
 
-def draw_job_plot2(test_log_path, output_file, log):
+def draw_job_plot2(test_log_path, output_file):
     if not os.path.exists(test_log_path):
-        log.error('test logfile not found!')
+        logging.error('test logfile not found!')
         return
-    log.info('plotting learning curve as png')
+    logging.info('plotting learning curve as png')
     plot_script = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'my_tools', 'progress_plot.py')
     process = subprocess.Popen(['python',
                                 plot_script,
@@ -144,14 +141,14 @@ def draw_job_plot2(test_log_path, output_file, log):
         time.sleep(2)
         returncode = process.returncode
     if returncode is not 0:
-        log.error('plotter return code: '+str(process.returncode))
-        log.error(output)
+        logging.error('plotter return code: '+str(process.returncode))
+        logging.error(output)
     else:
-        log.info('plotter exited successfully (code {})'.format(returncode))
+        logging.info('plotter exited successfully (code {})'.format(returncode))
     return output
 
 
-def draw_job_net(solver_path, output_file, log):
+def draw_job_net(solver_path, output_file, CAFFE_TOOL_PATH):
     # draw_net.py <netprototxt_filename> <out_img_filename>
     net_path = ''
     with open(solver_path, 'r') as search:
@@ -171,17 +168,16 @@ def draw_job_net(solver_path, output_file, log):
         time.sleep(2)
         returncode = process.returncode
     if returncode is not 0:
-        log.error('draw_net return code: ' + str(process.returncode))
-        log.error(output)
+        logging.error('draw_net return code: ' + str(process.returncode))
+        logging.error(output)
     else:
-        log.info('draw_net exited successfully (code {})'.format(returncode))
+        logging.info('draw_net exited successfully (code {})'.format(returncode))
     return output
 
 
-def generate_parsed_splitted_logs(caffe_log_file, job_output_dir, log):
+def generate_parsed_splitted_logs(caffe_log_file, job_output_dir):
     #./parse_log.sh <input_log> <output_path>
     script_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'caffe_tools', 'parse_log.sh')
-    #log.debug('calling \n{}\n{}\n{}'.format(script_path, caffe_log_file, job_output_dir))
     process = subprocess.Popen(['{} {} {}'.format(script_path, caffe_log_file, job_output_dir)],
                                shell=True,
                                stdout=subprocess.PIPE,
@@ -192,20 +188,20 @@ def generate_parsed_splitted_logs(caffe_log_file, job_output_dir, log):
         time.sleep(2)
         returncode = process.returncode
     if returncode is not 0:
-        log.error('parse_log return code: ' + str(process.returncode))
-        log.error(output.replace(': ', ':\n'))
+        logging.error('parse_log return code: ' + str(process.returncode))
+        logging.error(output.replace(': ', ':\n'))
     else:
-        log.info('parse_log exited successfully (code {})'.format(returncode, output))
+        logging.info('parse_log exited successfully (code {})'.format(returncode, output))
     return output
 
 
-def generate_output_directory(solver_path, net_path, snapshot_path, log):
+def generate_output_directory(solver_path, net_path, snapshot_path):
     if not os.path.exists(snapshot_path):
         os.makedirs(snapshot_path)
         # copy used settings and network to output dir
         shutil.copyfile(solver_path, os.path.join(snapshot_path, os.path.basename(solver_path)))
         shutil.copyfile(net_path, os.path.join(snapshot_path, os.path.basename(net_path)))
     else:
-        log.error('tmp directory is not empty! Aborting')
+        logging.error('tmp directory is not empty! Aborting')
         sys.exit()
     return snapshot_path
