f1a4a519d4136899363b53288d2bbc728851939e
Bytewright <bytewright@t-online.de>
2016-11-02 17:24:16 +0100

    mean visualsiation, new job testing

--- scripts/mean_visi.py
+++ scripts/mean_visi.py
@@ -10,14 +10,23 @@ import pylab
 model_path = '/home/ellerch/caffeProject/auto_trainer_output/2016-10-30_11h-46m-18s_experiment/vanilla_alexnet/deploy_alexnet_places365.prototxt'
 weights_path = '/home/ellerch/caffeProject/auto_trainer_output/2016-10-30_11h-46m-18s_experiment/vanilla_alexnet/_iter_110000.caffemodel'
 out_path = '/home/ellerch/caffeProject/mean_visi.png'
+mean_path = '/home/ellerch/db/places365/places365CNN_mean.binaryproto'
+
+#convert to npy
+blob = caffe.proto.caffe_pb2.BlobProto()
+data = open( mean_path , 'rb' ).read()
+blob.ParseFromString(data)
+mean_arr = np.array( caffe.io.blobproto_to_array(blob) )
 
 net = caffe.Classifier(model_path,
                        weights_path,
-                       channel_swap=(2, 1, 0), raw_scale=255)
+                       raw_scale=255)
+#channel_swap=(2, 1, 0),
 net_mean = caffe.Classifier(model_path,
                             weights_path,
-                            mean='/home/ellerch/db/places365/places365CNN_mean.binaryproto',
-                            channel_swap=(2, 1, 0), raw_scale=255)
+                            mean=mean_arr[0],
+                            raw_scale=255)
+#channel_swap=(2, 1, 0),
 # mean=np.load('/path/to/caffe/python/caffe/imagenet/ilsvrc_2012_mean.npy'),
 fake = np.ones((227, 227, 3))
 
