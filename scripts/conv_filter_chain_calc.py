86449e94c84f2ade932379589a95d5e9dc4afbd6
Bytewright <bytewright@t-online.de>
2016-11-03 22:26:22 +0100

    jobs for titan

--- scripts/conv_filter_chain_calc.py
+++ scripts/conv_filter_chain_calc.py
@@ -1,9 +1,9 @@
-conv_filter = [3]
-conv_stride = [2]
-padding =     [1]
+conv_filter = [5]
+conv_stride = [1]
+padding =     [2]
 #conv_filter = [11]
 #conv_stride = [4]
-size = 14
+size = 7
 # 256, 13, 13]
 output_vars = 0
 for F,P,S in zip(conv_filter,padding,conv_stride):
