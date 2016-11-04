2291e493f67d0e514e6ca95d94df13f0670a31b6
Bytewright <bytewright@t-online.de>
2016-11-04 12:35:12 +0100

    added top-5 acc layer

--- scripts/conv_filter_chain_calc.py
+++ scripts/conv_filter_chain_calc.py
@@ -1,9 +1,9 @@
 conv_filter = [3]
 conv_stride = [2]
-padding =     [1]
+padding =     [0]
 #conv_filter = [11]
 #conv_stride = [4]
-size = 14
+size = 7
 # 256, 13, 13]
 output_vars = 0
 for F,P,S in zip(conv_filter,padding,conv_stride):
