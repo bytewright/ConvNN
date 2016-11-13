52a44358b00e3abdbffdebddb68d9fd05d287af8
Bytewright <bytewright@t-online.de>
2016-11-13 22:21:14 +0100

    updates scripts

--- scripts/testing.py
+++ scripts/testing.py
@@ -1,41 +1,41 @@
-import os
-import json
-import time
+from PIL import Image
 import numpy as np
+import math
+import subprocess
+import os
 
-
-
-
-def load_data(data_file):
-    csv_delimiter = ';'
-    test_data = []
-    with open(data_file, 'r') as f:
-        for line in f.readlines():
-            #print line
-            line = line.rstrip()
-            if line.split(csv_delimiter).__len__() < 3:
-                print('incomplete line found')
-            else:
-                test_data.append([data for data in line.split(csv_delimiter) if data is not ''])
-
-    return test_data[1:]
 
 if __name__ == '__main__':
-    path = 'H:\\Thesis_data\\02_inception\\'
-    ref_path = 'H:\\00_vanilla\\'
-    for f in os.listdir(path):
-        if f.endswith(".json"):
-            json_file = os.path.join(path, f)
-        if f.endswith(".csv"):
-            csv_file = os.path.join(path, f)
-    for f in os.listdir(ref_path):
-        if f.endswith(".json"):
-            ref_file = os.path.join(ref_path, f)
-        if f.endswith(".csv"):
-            csv_ref_file = os.path.join(ref_path, f)
+    script_path = '/home/ellerch/bin/caffe/python/classify.py'
+    input_file = '/home/ellerch/caffeProject/scripts/classify_test/jcxdqj4r.bmp'
+    output_file = '/home/ellerch/caffeProject/scripts/classify_test/jcxdqj4r.output'
+    model_def = '/home/ellerch/caffeProject/web_interface/cnn/sce_vgg_16/sce_deploy_vgg16_places365.prototxt'
+    pretrained_model = '/home/ellerch/caffeProject/web_interface/cnn/sce_vgg_16/sce_vgg16_places365.caffemodel'
+    images_dim = '256,256'
+    mean_file = '/home/ellerch/db/places365/places365CNN_mean.binaryproto'
+    input_scale = 1.0
+    raw_scale = 255.0,
+    channel_swap = '2,1,0'
 
-    csv_data = load_data(csv_file)
-    csv_ref = load_data(csv_ref_file)
-    print get_max_accs(csv_data)
-    print get_avg_acc_1(csv_data, 10)
-    print get_avg_loss(csv_data, 10)
+    print 'plotting learning curve as png'
+    # plot_script = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'progress_plot.py')
+    process = subprocess.Popen(['python',
+                                script_path,
+                                input_file,
+                                output_file,
+                                '--model_def',
+                                model_def,
+                                '--pretrained_model',
+                                pretrained_model,
+                                '--gpu',
+                                '--images_dim',
+                                images_dim,
+                                "--mean_file", mean_file
+                                "--input_scale", input_scale,
+                                "--raw_scale", raw_scale,
+                                "--channel_swap", channel_swap],
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT)
+    # os.path.join(os.path.dirname(path), 'caffe_log_test{}.csv'.format(os.path.basename(path)[-5]))],
+    output = process.communicate()[0]
+    print output
